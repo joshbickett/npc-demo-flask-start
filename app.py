8a8e49d34e32faa588f107c7fdb93c88620a96d2
Josh Bickett <josh@bickett.net>
2023-08-26 17:30:28 -0700

    Add "hello world" response for `/chat` endpoint

--- app.py
+++ app.py
@@ -22,5 +22,24 @@ def index():
     """
     return "hello world"
 
+@app.route('/chat', methods=['GET', 'POST'])
+def chat():
+    """
+    This function handles the /chat endpoint, which is called by the frontend to send a message to the AI.
+    """
+    print('/chat called')
+    conversation = request.get_json().get('conversation', [])
+    print(conversation)
+
+    # If the conversation is more than 20 messages long, keep only the first and last 10
+    if len(conversation) > 20:
+        conversation = conversation[:10] + conversation[-10:]
+
+    # Create a list of messages for the OpenAI API, mapping 'human' to 'user' and 'ai' to 'assistant'
+    messages = [{"role": "user" if message['sender'] == "human" else "assistant", 
+                 "content": message['text']} for message in conversation]
+    
+    return jsonify({"response": "hello world", })
+
 if __name__ == '__main__':
     app.run(debug=True)
